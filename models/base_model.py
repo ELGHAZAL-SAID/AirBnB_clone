c54a355520f4b17822eef75584b71bdb4d029a8d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-11 15:25:34 +0100

    pycodestyle fixing

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,8 @@ class BaseModel():
         if kwargs:
             for key, value in kwargs.items():
                 if key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
         else:
