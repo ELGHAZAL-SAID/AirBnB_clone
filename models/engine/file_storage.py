3d243623afea4bc5e28b135f2675e3f8fa42a327
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-11 16:30:02 +0100

    serializing object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ import os.path
 
 
 class FileStorage():
+
     __file_path = ""
     __objects = {}
 
@@ -27,3 +28,19 @@ class FileStorage():
         dict = {}
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(dict, file)
+
+
+serialized_objects = {}  # Create an empty dictionary to store serialized objects
+
+# Iterate through each key-value pair in the FileStorage.__objects dictionary
+for key, obj in FileStorage.__objects.items():
+    # 'key' is the combination of class name and object ID, 'obj' is the object itself
+    
+    # Call the 'to_dict()' method on the object to serialize its attributes into a dictionary
+    serialized_data = obj.to_dict()
+    
+    # Use the 'key' as the dictionary key and the 'serialized_data' as the value
+    serialized_objects[key] = serialized_data
+
+# After the loop, 'serialized_objects' contains serialized representations of all objects
+# where each key is the unique identifier and the corresponding value is the serialized data
