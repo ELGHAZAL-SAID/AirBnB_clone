9a5cb203a4397035e330b2f10377651794102127
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-12 13:50:34 +0100

    fix pycodestyle

--- models/user.py
+++ models/user.py
@@ -6,6 +6,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
     email = ''
     password = ''
     first_name = ''
